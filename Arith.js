2f328ce360eefb7b6088610001894559915da7c4
Kazenzi <jacintakazenzi99@gmail.com>
2023-10-10 21:19:30 +0300

    mm

---
+++ Arith.js
@@ -0,0 +1,19 @@
+//Arithmetic Operator
+
+let x = 5;
+
+let y = 2;
+
+let sum = x + y;         // sum is 7
+
+let difference = x - y;  // difference is 3
+
+let product = x * y;     // product is 10
+
+let quotient = x / y;    // quotient is 2.5
+
+let remainder = x % y;   // remainder is 1
+
+x++;                     // x is now 6
+
+y--;                     // y is now 1
---
+++ obj.js
@@ -0,0 +1,17 @@
+const person = {
+
+    firstName: "John",
+
+    lastName: "Doe",
+
+    age: 50,
+
+    phoneNumber: "+13458696047",
+
+    isMarried: true
+
+}
+// Array
+const numberArray = [1, 2, 3, 4, 5, 6]
+
+const fruit = ["Banana", "Watermelon", "Orange", "Grape", "Pineapple"]
